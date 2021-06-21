6ebc0a0270b9efbe644daf1308f911677fae4b54
RJSouWal <ujjwalkakar2005@gmail.com>
2021-06-21 07:50:00 +0530

    Commit 1 for the Projects

---
+++ CalculatorBasic App.py
@@ -0,0 +1,17 @@
+#To iterate the app
+while True:
+    # Welcome Line
+    print("Hello,\n Welcome to Python Adder \n We will add, subtract and multiply any two numbers you give us")
+
+    # Number Entry as vaiables X and Y
+    x=int(input(' Please input a number \n '))
+    y=int(input(' Please input another number \n '))
+
+    # Print addition result statement
+    print(' Your Numbers are"',x,'"and"',y,'"and the sum is"',(x+y),'"\n')
+
+    #Print subtraction result Statement
+    print(' Your Numbers are"',x,'"and"',y,'"and the difference is"',(x-y),'"\n')
+
+    #Print product result statement
+    print(' Your Numbers are"',x,'"and"',y,'"and the product is"',(x*y),'" \n')
---
+++ hello world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+num = 0
+while 1==1:
+    pass
+    var = input('Enter 1 or 0 \n')
+    num = int(var)
+    5
+    if num==1:
+        print('You pressed one, it refers to true in boolean')
+    elif num==0:
+        print('You pressed zero, it refers to false in boolean')
+    else:
+        print("You did not press 1 or 0")
