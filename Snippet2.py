ce58fa9f8f7ab5a7dd724a8502c2b11b37e71192
RJSouWal <ujjwalkakar2005@gmail.com>
2021-06-24 09:40:25 +0530

    Commit 24/6/21 0940

--- Snippet2.py
+++ Snippet2.py
@@ -1,1 +1,9 @@
-    
+while True:
+    print('Please input two numbers to add.')
+    x = int(input('Please enter first number: \n'))
+    # x = int(numFilter(x))
+    y = int(input('Please enter second number: \n'))
+    # y = int(numFilter(y))
+
+    sum = x + y
+    print("The sum of ",x," and ",y," is ",sum, sep="", end="\n\n\n",)
