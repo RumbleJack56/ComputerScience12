f7ba07b37bed468b8297c807786fdc7bea181390
RJSouWal <ujjwalkakar2005@gmail.com>
2021-07-12 09:00:52 +0530

    12/07/2021 09:00

--- Project/Snippets/Snippet1.py
+++ Project/Snippets/Snippet1.py
@@ -1,1 +1,10 @@
-print("Github")
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+ax = a
+a+=b
+
+print("the value of a+b is",a)
+a = ax
+
+a-=b
+print("the value of a-b is",a)
