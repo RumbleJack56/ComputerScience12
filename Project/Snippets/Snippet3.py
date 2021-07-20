5fcb4176f22c91fd85330468c0f1cdd9b4013cd4
RJSouWal <ujjwalkakar2005@gmail.com>
2021-07-20 09:06:55 +0530

    20 07 2021 09:06

--- Project/Snippets/Snippet3.py
+++ Project/Snippets/Snippet3.py
@@ -1,2 +1,21 @@
 def Hi(name="Human"):
     print("Hello,",name,"I am Python interpreter")
+
+a=5
+print("a =",a)
+input()
+
+b=2 * a
+print("b =",b)
+input()
+
+a+=a+b
+print("a =",a)
+input()
+
+b*=a+b
+print("b =",b)
+input()
+
+
+print(a,b)
