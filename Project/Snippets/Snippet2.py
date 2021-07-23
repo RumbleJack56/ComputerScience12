5d924892f0bb91a8575c927eb8a60ce10f6d267e
RJSouWal <ujjwalkakar2005@gmail.com>
2021-07-23 12:02:53 +0530

    23 07 2021 12:02

---
+++ Project/Snippets/Snippet2.py
@@ -0,0 +1,25 @@
+# def calculate_area(b=1, h=1, shapeType=''):
+#     if (shapeType.lower() =='triangle'):
+#         ar = b * h / 2
+#         return ar
+#     elif (shapeType.lower() == 'rectangle'):
+#         ar = b * h
+#         return ar
+#     else:
+#         print("invalid input")
+#
+# base = int(input("Base: "))
+# height = int(input("Height: "))
+# Shape = input("Tell Shape (Rectangle or Triangle): ")
+#
+# areaR = calculate_area(base, height, Shape)
+#
+# print(areaR)
+##########################################################################
+
+def convertDollar(amount_in_dollar , dollar_to_rupee):
+    amount_in_rupee = amount_in_dollar * dollar_to_rupee
+    return amount_in_rupee
+
+x = convertDollar(int(input("Enter Amount in Dollars: ")),int(input("Enter Dollar to rupee convertion rate: ")))
+print("Value in rupee is",x)
---
+++ Project/Snippets/Snippet4.py
@@ -0,0 +1,14 @@
+print("First 5 Multiples")
+
+a = int(input("Input your Number: "))
+print("The first five multiples of",a,"are-")
+x = a
+print(x)
+x+=a
+print(x)
+x+=a
+print(x)
+x+=a
+print(x)
+x+=a
+print(x)
