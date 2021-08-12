5e4373571041e5a89bc9c86fa06b0c71412ffeb8
RJSouWal <ujjwalkakar2005@gmail.com>
2021-08-12 09:02:24 +0530

    12/08/2021 09:02

--- Project/Snippets/Snippet3.py
+++ Project/Snippets/Snippet3.py
@@ -1,9 +1,11 @@
-p=5%2            #1
-q=p**4           #1
-r=p//q           #1
-p+=p+q+r         #1 + 1 + 1+1 =4
-r+=p+q+r         #1 + 4 + 1+ 1 = 7
-q-=p+q*r         #1 - 4 - 7 = -10
-print(p,q,r)
+def sorter(variable_type_list=[]):
+    lis = variable_list
+    lisLen=len(lis)
+    for a in range(0,lisLen):
+        for iter in range(a+1,lisLen):
+            if  lis[a] > lis[iter]:
+                lis[a],lis[iter] = lis[iter],lis[a]
+    print(lis)
 
-#Vartika is Great
+toArrange = [123,4353,232,434,9948,18,1,43,56,234]
+sorter(toArrange)
--- Project/Snippets/Snippet4.py
+++ Project/Snippets/Snippet4.py
@@ -1,14 +1,1 @@
-print("First 5 Multiples")
-
-a = int(input("Input your Number: "))
-print("The first five multiples of",a,"are-")
-x = a
-print(x)
-x+=a
-print(x)
-x+=a
-print(x)
-x+=a
-print(x)
-x+=a
-print(x)
+print(int(input("Enter Binary: "),2))
