a9c091c9951a8b9c0d27d7149e7f9992f3017777
RJSouWal <ujjwalkakar2005@gmail.com>
2021-08-27 08:58:33 +0530

    27 08 2021 08:58

--- Project/Practicals/pr25.py
+++ Project/Practicals/pr25.py
@@ -1,10 +1,10 @@
-print("String Mutation \n  The program will remove vowels and consonants")
+print("String Mutation \n  The program will remove vowels and uppercase the consonants")
 Sen , ts=input("Enter a few Lines: \n") , []
 for j in range(0,len(Sen)):
     ts.append(Sen[j])
 for num in range(0,len(Sen)):
-    a=ts[num].lower
-    if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u"):
+    a=ts[num]
+    if(a=="a" or a=="A" or a=="e" or a=="E" or a=="i" or a=="I" or a=="o" or a=="O" or a=="u" or a=="U"):
         ts[num] = ""
     else:
         ts[num] = ts[num].upper()
--- Project/Snippets/Snippet1.py
+++ Project/Snippets/Snippet1.py
@@ -1,1 +1,17 @@
-print("Title Case of Given String is:\n",(input("Title Case \nEnter String for Conversion: ").title()))
+# str1="safetylaws"
+# l=len(str1)
+# str2=""
+# for i in range(0,l):
+#     if(i%2==0):
+#         str2+=str1[i].upper()
+#     else:
+#         str2+=str1[i].lower()
+# print("String after Capitalising is : ",str2)
+
+str1="safetylaws"
+print("The String after Capitalising is : ",end="")
+for i in range(0,len(str1)):
+    if(i%2==0):
+        print(str1[i].upper(),end="")
+    else:
+        print(str1[i].lower(),end='')
