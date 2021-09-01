40e8a232ac316037b119922ea9e784fe51b3290e
RJSouWal <ujjwalkakar2005@gmail.com>
2021-09-01 08:49:56 +0530

    01 09 2021 08:49

--- Project/Practicals/pr25.py
+++ Project/Practicals/pr25.py
@@ -4,7 +4,7 @@ for j in range(0,len(Sen)):
     ts.append(Sen[j])
 for num in range(0,len(Sen)):
     a=ts[num]
-    if(a=="a" or a=="A" or a=="e" or a=="E" or a=="i" or a=="I" or a=="o" or a=="O" or a=="u" or a=="U"):
+    if a in "AEIOUaeiou":
         ts[num] = ""
     else:
         ts[num] = ts[num].upper()
