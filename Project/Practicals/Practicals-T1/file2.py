f5367f8f59d19ff504b1c2868ee02904544bcd99
RJSouWal <ujjwalkakar2005@gmail.com>
2021-11-02 09:39:34 +0530

    02/11/2021 09:39

---
+++ Project/Practicals/Practicals-T1/file2.py
@@ -0,0 +1,6 @@
+a = [1 , 3 , 73 , 23 , 54 , 32 , 1]
+max = a[0]
+for j in a:
+    if j > max:
+        max = j
+print('Largest number in',a,'is',max)
---
+++ Project/Practicals/Practicals-T1/file3.py
@@ -0,0 +1,5 @@
+n , a = input("Enter String:") , 0
+for j in n:
+    if j in "1234567890":
+        a+=int(j)
+print("Result is",a)
