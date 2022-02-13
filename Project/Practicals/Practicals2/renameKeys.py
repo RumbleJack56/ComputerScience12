bed9c2f20c26371278db48d8383b1e6e5aa487c2
RJSouWal <57482148+RJSouWal@users.noreply.github.com>
2022-02-13 22:39:43 +0530

    edited on chrome

--- Project/Practicals/Practicals2/renameKeys.py
+++ Project/Practicals/Practicals2/renameKeys.py
@@ -24,3 +24,7 @@ print(renameKey2({1:'a', 2:'b' , 3:'c'} , 2 , 6))
 #Advanced
 renameKey3 = lambda d , k1 , k2 : dict([(k2,x[1]) if x[0]==k1 else (x[0],x[1]) for x in d.items()])
 print(renameKey3({1:'a', 2:'b' , 3:'c'} , 2 , 6))
+
+
+
+print("LOL")
