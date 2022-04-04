7330af076c7c2946178a7bc60c4035db7cdca3a9
RJSouWal <ujjwalkakar2005@gmail.com>
2022-04-04 17:04:52 +0530

    04 04 2022 17:04

--- classXII/snippets/Snippet1.py
+++ classXII/snippets/Snippet1.py
@@ -1,13 +1,18 @@
-a = "H ;4 t akos"
-out = ''
-for j in a:
-    if j.isspace():
-        out+=j
-    elif j.isdigit():
-        out+=str(int(j) - 1)
-    else:
-        if j in 'aeiou':
-            out+=j
-        else:
-            out+=chr( ord(j) + 1 )
-print(out)
+class Teacher:
+    retired = False
+    
+    def __init__(self, subject=None, age=None , designation=None):
+        self.subject = subject
+        self.age = age
+        self.designation = designation
+
+rajni = Teacher("science",33,"TGT")
+shyamal = Teacher("Maths", 912, "Voldemort")
+gurjeet_maam = Teacher("CS" , 34 , "PGT")
+
+for x in dir():
+    y = eval(x)
+    if type(y) == type(Teacher()):
+        print("Teacher is",x,"who is a",y.subject,"teacher and is aged",y.age,".",
+        
+        "Their designation is",y.designation,".")
