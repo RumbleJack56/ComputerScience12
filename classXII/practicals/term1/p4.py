43cb668f812cf739dd28121b8536f2064bd5663f
RJSouWal <ujjwalkakar2005@gmail.com>
2022-04-25 19:59:44 +0530

    25 04 2022 19:59

--- classXII/practicals/term1/p4.py
+++ classXII/practicals/term1/p4.py
@@ -12,14 +12,10 @@ def simpleCalc():
     cv = conNum(input("Enter number: "))
     while True:
         fun = input("Enter operation: ")
-
-        if (fun == "+"): 
-    if (fun == "+"): 
+ 
         if (fun == "+"): 
             cv += conNum(input("Enter number: "))
             print(cv)
-        if (fun == "-"): 
-    if (fun == "-"): 
         if (fun == "-"): 
             cv -= conNum(input("Enter number: "))
             print(cv)
@@ -27,12 +23,8 @@ def simpleCalc():
             cv *= conNum(input("Enter number: "))
             print(cv)
         if (fun == "/"): 
-    if (fun == "/"): 
-        if (fun == "/"): 
             cv /= conNum(input("Enter number: "))
             print(cv)
-        if (fun == "^"): 
-    if (fun == "^"): 
         if (fun == "^"): 
             v = conNum(input("Enter number: "))
             cv=pow(cv,v)
@@ -98,17 +90,79 @@ def SAcalc():
         print("The Surface Area of Frustrum is  ",
         (conNum(input("Enter radius 1: ")) + conNum(input("Enter radius 2: "))) * pi * (conNum(input("Enter height: ")) + 2) )
 
+def TrigCalc():
+    a = lambda x,y: x+" of "+str(y)+" is "+str(eval(x+"("+str(y)+")"))
+    print("Welcome to trignometric calculator, What operation would you like to perform?")
+    trigOp = input("Enter function(sin,cos,tan,asin,acos,atan): ").lower()
+    if trigOp == "sin": print(a(trigOp , float(input("Enter Value: "))))
+    elif trigOp == "cos": print(a(trigOp , float(input("Enter Value: "))))
+    elif trigOp == "tan": print(a(trigOp , float(input("Enter Value: "))))
+    elif trigOp == "asin": print(a(trigOp , float(input("Enter Value: "))))
+    elif trigOp == "acos": print(a(trigOp , float(input("Enter Value: "))))
+    elif trigOp == "atan": print(a(trigOp , float(input("Enter Value: "))))
 
+def CashCalc():
+    print("Welcome to Currency convertor: it converts the currencies to different forms")
+    base = input("Enter a supported currency(USD,EUR,JPY,GBP,AUD,CAD,INR): ").upper()
+    val = float(input("Enter amount:"))
+    convertto = input("Enter 2nd supported currency(USD,EUR,JPY,GBP,AUD,CAD,INR): ").upper()
+    if base=="USD":
+        if convertto=="USD":print("Exchange Value is",val*1)
+        if convertto=="EUR":print("Exchange Value is",val*0.93)
+        if convertto=="JPY":print("Exchange Value is",val*127.71)
+        if convertto=="GBP":print("Exchange Value is",val*0.78)
+        if convertto=="AUD":print("Exchange Value is",val*1.4)
+        if convertto=="CAD":print("Exchange Value is",val*1.27)
+        if convertto=="INR":print("Exchange Value is",val*76.69)
+    if base=="EUR":
+        if convertto=="USD":print("Exchange Value is",val*1.07)
+        if convertto=="EUR":print("Exchange Value is",val*1)
+        if convertto=="JPY":print("Exchange Value is",val*136.97)
+        if convertto=="GBP":print("Exchange Value is",val*0.84)
+        if convertto=="AUD":print("Exchange Value is",val*1.5)
+        if convertto=="CAD":print("Exchange Value is",val*1.37)
+        if convertto=="INR":print("Exchange Value is",val*82.17)
+    if base=="JPY":
+        if convertto=="USD":print("Exchange Value is",val*0.0078)
+        if convertto=="EUR":print("Exchange Value is",val*0.0073)
+        if convertto=="JPY":print("Exchange Value is",val*1)
+        if convertto=="GBP":print("Exchange Value is",val*0.0062)
+        if convertto=="AUD":print("Exchange Value is",val*0.011)
+        if convertto=="CAD":print("Exchange Value is",val*0.010)
+        if convertto=="INR":print("Exchange Value is",val*0.60)
+    if base=="GBP":
+        if convertto=="USD":print("Exchange Value is",val*1.27)
+        if convertto=="EUR":print("Exchange Value is",val*1.19)
+        if convertto=="JPY":print("Exchange Value is",val*162.54)
+        if convertto=="GBP":print("Exchange Value is",val*1)
+        if convertto=="AUD":print("Exchange Value is",val*1.78)
+        if convertto=="CAD":print("Exchange Value is",val*1.62)
+        if convertto=="INR":print("Exchange Value is",val*97.62)
+    if base=="AUD":
+        if convertto=="USD":print("Exchange Value is",val*0.71)
+        if convertto=="EUR":print("Exchange Value is",val*0.67)
+        if convertto=="JPY":print("Exchange Value is",val*91.2)
+        if convertto=="GBP":print("Exchange Value is",val*0.56)
+        if convertto=="AUD":print("Exchange Value is",val*1)
+        if convertto=="CAD":print("Exchange Value is",val*0.91)
+        if convertto=="INR":print("Exchange Value is",val*54.77)
 
-
-
-
-        
-
-
-
-
-
+    if base=="CAD":
+        if convertto=="USD":print("Exchange Value is",val*0.78)
+        if convertto=="EUR":print("Exchange Value is",val*0.73)
+        if convertto=="JPY":print("Exchange Value is",val*100.04)
+        if convertto=="GBP":print("Exchange Value is",val*0.62)
+        if convertto=="AUD":print("Exchange Value is",val*1.10)
+        if convertto=="CAD":print("Exchange Value is",val*1)
+        if convertto=="INR":print("Exchange Value is",val*60.08)
+    if base=="INR":
+        if convertto=="USD":print("Exchange Value is",val*0.013)
+        if convertto=="EUR":print("Exchange Value is",val*0.012)
+        if convertto=="JPY":print("Exchange Value is",val*1.67)
+        if convertto=="GBP":print("Exchange Value is",val*0.010)
+        if convertto=="AUD":print("Exchange Value is",val*0.018)
+        if convertto=="CAD":print("Exchange Value is",val*0.017)
+        if convertto=="INR":print("Exchange Value is",val*1)
 
 
 
@@ -122,11 +176,11 @@ while True:
     if optionType.lower() == "volume" or optionType.lower() == "1":
         VolumeCalc()
     if optionType.lower() == "surface area" or optionType.lower() == "2":
-        pass
+        SAcalc()
     if optionType.lower() == "trigonometry" or optionType.lower() == "3":
-        pass
+        TrigCalc()
     if optionType.lower() == "currency" or optionType.lower() == "4":
-        pass
+        CashCalc()
     if optionType.lower() == "general" or optionType.lower() == "5":
         simpleCalc()
     if optionType.lower() == "exit" or optionType.lower() == "6":
