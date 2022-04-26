9014e017bfc43a2f56e8bf9d352e0ebc951dc61c
RJSouWal <ujjwalkakar2005@gmail.com>
2022-04-26 08:27:06 +0530

    26 04 2022 08:25

--- classXII/snippets/Snippet1.py
+++ classXII/snippets/Snippet1.py
@@ -1,34 +1,3 @@
-from math import *
-
-def conNum(x):
-    if float(x) == int(x): 
-        return eval(x)
-    elif float(x) != int(x): 
-        return float(x)
-
-print("Welcome to Calculator, + adds , - subtracts , x multiplies , / divides , ^ exponents")
-cv = conNum(input("Enter number: "))
-while True:
-    fun = input("Enter operation: ")
-
-    if (fun == "+"): 
-        cv += conNum(input("Enter number: "))
-        print(cv)
-    
-    if (fun == "-"): 
-        cv -= conNum(input("Enter number: "))
-        print(cv)
-
-    if (fun.lower() == "x"): 
-        cv *= conNum(input("Enter number: "))
-        print(cv)
-
-    if (fun == "/"): 
-        cv /= conNum(input("Enter number: "))
-        print(cv)
-
-    if (fun == "^"): 
-        v = conNum(input("Enter number: "))
-        cv=pow(cv,v)
-        print(cv)
-        
+name = "Madam"
+print(name.replace('m','nna').upper())
+print(name.upper().replace("M",'NNA'))
