4a7e69c43ceb16d4ff7baf2da9fa6ca883589eb8
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-05 11:37:28 +0530

    05 07 2022 11:37

---
+++ classXII/practicals/term1/p33.py
@@ -0,0 +1,14 @@
+#7/5/22
+
+import sd
+
+myfile=open('p33.txt','a')
+
+for i in range(2):
+    name=input('Enter a name to store in the file: ')
+    myfile.write(name)
+    myfile.write('\n')
+
+myfile.close()
+
+print('Data saved in the file.')
