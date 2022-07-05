9485f1e24a48c43e2d42b2857765da9389e32768
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-05 11:09:53 +0530

    05 07 2022 11:09

--- classXII/practicals/term1/pr29.py
+++ classXII/practicals/term1/pr29.py
@@ -1,1 +1,26 @@
-file 
+import sd
+
+#method 1 to read
+myfile1 = open("pr29.txt", "r")
+for line in myfile1:
+    print(line,end="")
+print()
+myfile1.close()
+
+
+#method 2 to read
+myfile1 = open("pr29.txt", "r")
+for line in myfile1.readlines():
+    print(line.rstrip("\n"))
+
+myfile1.close()
+
+
+#method 3 to read
+myfile1 = open("pr29.txt", "r")
+a = [line[:-1] for line in myfile1]
+print(a)
+myfile1.close()
+
+
+
