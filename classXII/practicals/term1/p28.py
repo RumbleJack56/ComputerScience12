8e67d296cc46b72c314f5e47159696ce44e65f3b
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-05 10:41:10 +0530

    05 07 2022 10:41

---
+++ classXII/practicals/term1/p28.py
@@ -0,0 +1,17 @@
+#7/5/22
+
+import sd
+
+myfile=open('pr28.txt','r')
+str1=' '
+size=0
+tsize=0
+
+while str1:
+    str1=myfile.readline()
+    tsize=tsize+len(str1)
+    size=size+len(str1.strip())
+
+print('Total size: ',tsize)
+print('Size after removing EOL and blank spaces: ',size)
+myfile.close()
