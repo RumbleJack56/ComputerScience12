c8ab19bf4ce660db4b752cfa715a6920023fd1be
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-06 13:13:30 +0530

    06 07 2022 13:13

--- classXII/practicals/term1/p37.py
+++ classXII/practicals/term1/p37.py
@@ -6,6 +6,15 @@ myfile=open('p37.txt','w+')
 
 myfile.writelines('Hye')
 myfile.flush()
+input("Next: ")
+myfile.writelines('\nThe return of Hi')
+myfile.flush()
+input("Next: ")
+myfile.writelines('\nHi, the wrath of Hello')
+myfile.flush()
+input("Next: ")
+myfile.writelines('\nThe last Hi')
+myfile.close()
 
 
 print('Data saved in the file.')
