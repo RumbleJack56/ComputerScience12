44dce1e9125a9f15edd2bb0b62b7abd94a0e0dcf
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-06 12:50:26 +0530

    06 07 2022 12:50

---
+++ classXII/practicals/term1/p35.py
@@ -0,0 +1,28 @@
+#06-07-2022
+#file copy one from another
+import sd
+
+#method1
+file1 = open("p35a.txt", 'r')
+file2 = open("p35b.txt", 'w')
+
+
+str = " "
+while str:
+    str = file1.readline()
+    file2.write(str)
+
+
+file1.close()
+file2.close()
+
+#method2
+file1 = open("p35a.txt", 'r')
+file2 = open("p35b.txt", 'w')
+
+
+[file2.write(line) for line in file1.readlines()]
+
+
+file1.close()
+file2.close()
