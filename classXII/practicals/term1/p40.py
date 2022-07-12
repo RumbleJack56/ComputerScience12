e011b1af8f7f4a1170abfb3fef88b064489e9e5c
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-12 11:09:31 +0530

    12 07 2022 11:09

--- classXII/practicals/term1/p40.py
+++ classXII/practicals/term1/p40.py
@@ -3,5 +3,8 @@
 import sd
 from pickle import *
 
-with open('p39.txt', 'r+b') as f:
-    pass
+with open('p40.txt', 'w+b') as f:
+    dump({1:'p',2:'b',3:'c',4:'d'},f)
+    f.seek(0)
+    data = load(f)
+    print(data)
