8dad2663f735b0292365e42fd8c4bbe77b77db6c
RJSouWal <ujjwalkakar2005@gmail.com>
2022-07-21 10:40:06 +0530

    21 07 2022 10:40

--- classXII/practicals/term1/p48.py
+++ classXII/practicals/term1/p48.py
@@ -2,32 +2,42 @@ import sd
 from pickle import *
 
 class student:
-    def __init__(self):
-        pass
-class Collection:
+    def assignrno(self,cls, sec, col):
 
+
+    def __init__(self,):
+        
+class Collection:
     def __init__(self,dirpath):
         self.dirpath = dirpath
 
+
         def filecheck(location):
-            try: a = load(open(location,'rb')); a.close()
-            except: a = open(location,'wb') ; dump([],a)
-        
+            try: a = load(open(location,'rb'))
+            except: a = open(location,'wb') ; dump([],a) ; print("Exception occurs")
         filecheck(dirpath)
-        def filerefresh():
-            with open(dirpath,"rb") as f1:
-                self.data = load(f1)
-                self.totalnum = len(self.data)
-        filerefresh()
+
+
+        #basis filerefresh
+        with open(dirpath,"rb") as f1:
+            self.data = load(f1)
+            self.totalnum = len(self.data)
+            print(self.data)
+
+    def filerefresh(self):
+        with open(self.dirpath,"rb") as f1:
+            self.data = load(f1)
+            self.totalnum = len(self.data)
 
     def add(self,info):
         self.data+=[info]
         with open(self.dirpath,'wb') as f1:
             dump(self.data,f1)
+        self.filerefresh()
 
 
 students = Collection('p48.txt')
+
 students.add('Ujjwal')
-students.add("Hardik")
 print(students.data)
         
