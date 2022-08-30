dcbcc16045b6553aacea137be94e65f5bbf5ee0f
RJSouWal <ujjwalkakar2005@gmail.com>
2022-08-30 11:48:29 +0530

    30 08 2022 11:48

---
+++ classXII/db/flushdb.bat
@@ -0,0 +1,3 @@
+cd "C:\Program Files\MySQL\MySQL Server 5.7\bin"
+.\mysqldump -u root -p vartika > "E:\NEPS\School Books\CS\GIT_IT\classXII\db\vartika.sql"
+
---
+++ classXII/db/loaddb.bat
@@ -0,0 +1,2 @@
+cd "C:\Program Files\MySQL\MySQL Server 5.7\bin"
+.\mysqldump -u root -p vartika < "E:\NEPS\School Books\CS\GIT_IT\classXII\db\vartika.sql"
