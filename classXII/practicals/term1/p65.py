34d2c64f09eaa0e8983f2efd3ced95ac8f57d32b
RJSouWal <ujjwalkakar2005@gmail.com>
2022-08-30 10:44:32 +0530

    30 08 2022 10:44

--- classXII/practicals/term1/p65.py
+++ classXII/practicals/term1/p65.py
@@ -1,1 +1,15 @@
 #15-08-22
+import mysql.connector
+db = mysql.connector.connect(host ="localhost", user ="root", password ="")
+mq = db.cursor()
+
+mq.execute("SHOW DATABASES")
+val = mq.fetchall()
+print(val)
+if ("vartika",) not in val:
+    mq.execute("CREATE DATABASE vartika")
+
+
+mq.execute("SHOW DATABASES")
+print(mq.fetchall())
+
